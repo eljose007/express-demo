bac027c473e90d02494aaefd8dbb1b22d5fac856
Jose Eduardo Gonzalez <jogonzalez@mides.interno>
2018-12-21 16:35:29 -0300

    cambios

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 2, name: 'course3'}
+];
+
 app.get('/', (req, res) => {
     res.send('Hello Worldddd');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.query);
+app.post('/api/courses', (req, res) => {
+
+    if (!req.body.name || req.body.name.length < 3){
+        //400 bad request
+        res.status(400).send('Name is required and should be minimum 3 characters');
+        return;
+    }
+
+    const course = {
+        id: courses.length +1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) // 404
+        res.status(404).send('No se encuentra el curso');
+    res.send(course);
 });
 
 //probando comentario
